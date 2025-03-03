9778e021c2ee81727b1a973ca7ab20d553d61acc
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-03-03 21:54:31 +0600

    added navbar for mobile and large devices

--- src/components/custom-components/nav-bar.jsx
+++ src/components/custom-components/nav-bar.jsx
@@ -1,15 +1,41 @@
 import { Link } from "react-router";
 import { Button } from "../ui/button";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
+import { useTheme } from "@/providers/theme-provider";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useState } from "react";
 const NavBar = () => {
+  const { theme, setTheme } = useTheme();
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <nav className="inline-flex items-center gap-10 py-2 px-5 border rounded-lg bg-secondary/50 backdrop-blur-sm mt-5 fixed left-1/2 -translate-x-1/2">
+    <nav className="inline-flex items-center gap-10 py-1.5 px-5 border rounded-lg bg-secondary/50 backdrop-blur-sm mt-5 fixed left-1/2 -translate-x-1/2">
+      {/* Brand Logo */}
       <Link>LOGO</Link>
-      <DarkModeSwitch size={20} />
-      <div className="lg:hidden">
-        <Menu />
-      </div>
+
+      {/* Theme switcher */}
+      <Tooltip>
+        <TooltipTrigger>
+          <DarkModeSwitch
+            size={20}
+            checked={theme === "dark"}
+            onChange={() => {
+              setTheme(theme === "light" ? "dark" : "light");
+            }}
+          />
+        </TooltipTrigger>
+        <TooltipContent>Change theme</TooltipContent>
+      </Tooltip>
+
+      {/* Nav Links */}
       <div className="hidden lg:inline-flex gap-5 items-center">
         <Link> Home </Link>
         <Link> About </Link>
@@ -18,6 +44,35 @@ const NavBar = () => {
           <Button>Book a Call</Button>
         </Link>
       </div>
+
+      {/* Drawer for small devices */}
+      <Drawer direction="top" dismissible={false} open={drawerOpen}>
+        <DrawerTrigger
+          onClick={() => setDrawerOpen(true)}
+          className="lg:hidden"
+        >
+          <Menu />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle className="inline-flex flex-col gap-3 items-center pb-5">
+              <Link>Home</Link>
+              <Link>Route1</Link>
+              <Link>Route2</Link>
+              <Link>Route3</Link>
+            </DrawerTitle>
+            <DrawerDescription className="absolute left-1/2 -translate-x-1/2 -bottom-4">
+              <Button
+                variant="destructive"
+                size="icon"
+                onClick={() => setDrawerOpen(false)}
+              >
+                <X />
+              </Button>
+            </DrawerDescription>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
     </nav>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
+      <TooltipProvider delayDuration={200}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>
 );
